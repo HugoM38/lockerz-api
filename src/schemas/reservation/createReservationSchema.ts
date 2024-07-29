ba1196f05132bbb492f3beb58424fcd66fb68bdb
Hugo <>
2024-07-29 16:42:04 +0200

    fix: add limite of members in schema

--- src/schemas/reservation/createReservationSchema.ts
+++ src/schemas/reservation/createReservationSchema.ts
@@ -2,5 +2,5 @@ import Joi from "joi";
 
 export const createReservationSchema = Joi.object({
     lockerId: Joi.string().required(),
-    members: Joi.array().required()
+    members: Joi.array().min(1).max(4).required()
 });

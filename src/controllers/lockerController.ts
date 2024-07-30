be63a3df5b3e5d71b38beb78e73ce04eebe97818
Hugo <>
2024-07-30 22:28:44 +0200

    feat: sort locker when returned

--- src/controllers/lockerController.ts
+++ src/controllers/lockerController.ts
@@ -36,7 +36,8 @@ const getLockers = async (req: Request, res: Response) => {
   try {
     const lockers = await Locker.find()
         .select("_id number localisation status")
-        .populate("localisation");
+        .populate("localisation")
+        .sort({ number: 1 });
     res.status(200).json(lockers);
   } catch (error) {
     res.status(400).json({ error: "Une erreur inconnue s'est produite" });
